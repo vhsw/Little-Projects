b0da33c08c1dfc03b2d4b5b3ad07619f07008326
Alexey Dubrov <vhsw@ya.ru>
2020-04-16 21:15:12 +0300

    clean

--- corovaminus/src/components/erf.js
+++
@@ -1,6 +0,0 @@
-import { erf } from 'mathjs';
-
-export default function ([amp, offset, sigma]) {
-    let day_ms = 60 * 60 * 24 * 1000;
-    return (t) => amp / 2 * (erf((t - offset) / (sigma * 0.34 * day_ms)) + 1)
-}
